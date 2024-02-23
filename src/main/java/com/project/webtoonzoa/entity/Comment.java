0ff356136cac4effcbb38a6eec67f82f13805a1c
yiyaaa <thdtjs1219@gmail.com>
2024-02-23 16:53:25 +0900

    feat: Entity 구현

---
+++ src/main/java/com/project/webtoonzoa/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.project.webtoonzoa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SQLRestriction("deleted_at is NULL")
+public class Comment extends TimeStamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "webtonn_id")
+    private Webtoon webtoon;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
---
+++ src/main/java/com/project/webtoonzoa/entity/WebtoonLikes.java
@@ -0,0 +1,29 @@
+package com.project.webtoonzoa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class WebtoonLikes extends TimeStamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "webtoon_id")
+    private Webtoon webtoon;
+}
