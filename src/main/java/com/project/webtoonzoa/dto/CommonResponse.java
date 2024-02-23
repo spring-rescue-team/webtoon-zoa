589720da2e256da499092c0913d92d7c64ad1aa7
yiyaaa <thdtjs1219@gmail.com>
2024-02-24 00:55:25 +0900

    feat: 관리자, 일반권한 추가

--- src/main/java/com/project/webtoonzoa/dto/CommonResponse.java
+++ src/main/java/com/project/webtoonzoa/dto/CommonResponse.java
@@ -2,7 +2,9 @@ package com.project.webtoonzoa.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommonResponse<T> {
---
+++ src/main/java/com/project/webtoonzoa/global/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,89 @@
+package com.project.webtoonzoa.global.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.webtoonzoa.dto.UserRequestDto;
+import com.project.webtoonzoa.entity.Enum.UserRoleEnum;
+import com.project.webtoonzoa.global.util.JwtUtil;
+import com.project.webtoonzoa.global.util.UserDetailsImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j(topic = "로그인 및 JWT 생성")
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+        setFilterProcessesUrl("/users/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+            HttpServletResponse response) throws AuthenticationException {
+        log.info("로그인 시도");
+        try {
+            UserRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(),
+                    UserRequestDto.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getEmail(),
+                            requestDto.getPassword(),
+                            null
+                    )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+            HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+        log.info("로그인 성공 및 JWT 생성");
+        UserDetailsImpl user = (UserDetailsImpl) authResult.getPrincipal();
+        String username = user.getUsername();
+        UserRoleEnum role = user.getUser().getRole();
+
+
+        String token = jwtUtil.createToken(username,role);
+        jwtUtil.addJwtToCookie(token, response);
+
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("username", user.getUsername());
+
+        // JSON으로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(responseData);
+
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(jsonResponse);
+        response.getWriter().flush();
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request,
+            HttpServletResponse response, AuthenticationException failed)
+            throws IOException, ServletException {
+        log.error("로그인 실패");
+
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.getWriter().write("user not found");
+    }
+}
