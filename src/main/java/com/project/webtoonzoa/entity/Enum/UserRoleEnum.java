c9eb0b4eb816602dae3cf3eee515b6464cdcb472
yiyaaa <thdtjs1219@gmail.com>
2024-02-24 00:55:25 +0900

    feat: 관리자, 일반권한 추가

--- src/main/java/com/project/webtoonzoa/entity/Enum/UserRoleEnum.java
+++ src/main/java/com/project/webtoonzoa/entity/Enum/UserRoleEnum.java
@@ -1,8 +1,8 @@
 package com.project.webtoonzoa.entity.Enum;
 
 public enum UserRoleEnum {
-    USER(Authority.USER),  // 사용자 권한
-    ADMIN(Authority.ADMIN);  // 관리자 권한
+    USER("ROLE_USER"),  // 사용자 권한
+    ADMIN("ROLE_ADMIN");  // 관리자 권한
 
     private final String authority;
 
@@ -12,10 +12,5 @@ public enum UserRoleEnum {
 
     public String getAuthority() {
         return this.authority;
-    }
-
-    public static class Authority {
-        public static final String USER = "ROLE_USER";
-        public static final String ADMIN = "ROLE_ADMIN";
     }
 }
