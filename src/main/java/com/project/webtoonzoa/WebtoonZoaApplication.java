f183fe353a882261b47b468da95fd1e0f5686cf4
yiyaaa <thdtjs1219@gmail.com>
2024-02-23 15:33:45 +0900

    feat: git init

---
+++ src/main/java/com/project/webtoonzoa/WebtoonZoaApplication.java
@@ -0,0 +1,13 @@
+package com.project.webtoonzoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonZoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebtoonZoaApplication.class, args);
+    }
+
+}
