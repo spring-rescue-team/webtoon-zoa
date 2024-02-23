9cf94f306eb21c34f36c41eab6f11ad4cf4087b1
yiyaaa <thdtjs1219@gmail.com>
2024-02-23 17:23:11 +0900

    feat: 공통규약 설정

---
+++ src/main/java/com/project/webtoonzoa/global/exception/ControllerAdvice.java
@@ -0,0 +1,10 @@
+package com.project.webtoonzoa.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j(topic = "Exception")
+@RestControllerAdvice
+public class ControllerAdvice {
+
+}
