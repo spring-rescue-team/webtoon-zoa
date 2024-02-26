46071c24984d06763d85d4b9235d258c76a14001
yiyaaa <65538799+RamuneOrch@users.noreply.github.com>
2024-02-26 10:47:34 +0900

    Feat: user기능 구현, test 코드 작성 (#3)
    * feat: 관리자, 일반권한 추가
    
    * feat: 예외처리 양식 추가
    
    * feat: 로그아웃, 로그인 추가
    
    * refactor: 코드 컨벤션
    
    * feat: user info update 구현
    
    * test: 회원가입, 회원정보 수정 테스트 구현
    
    * refactor: 코드 컨벤션
    
    * feat: password update 기능 구현, 예외처리 추가
    
    * test: password update test 구현
    
    * refactor: 코드 컨벤션

---
+++ src/main/java/com/project/webtoonzoa/global/exception/PasswordNotEqualException.java
@@ -0,0 +1,8 @@
+package com.project.webtoonzoa.global.exception;
+
+public class PasswordNotEqualException extends IllegalStateException {
+
+    public PasswordNotEqualException(String message) {
+        super(message);
+    }
+}
