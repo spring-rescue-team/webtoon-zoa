d807a0ac7402cac43e92ef40d1677246a0e14e33
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-02-28 17:59:34 +0900

    feat: 대댓글 기능 구현 (#11)

--- src/main/java/com/project/webtoonzoa/repository/CommentRepository.java
+++ src/main/java/com/project/webtoonzoa/repository/CommentRepository.java
@@ -3,9 +3,11 @@ package com.project.webtoonzoa.repository;
 import com.project.webtoonzoa.entity.Comment;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-
-    List<Comment> findByWebtoonIdAndDeletedAtIsNullOrderByCreatedAtAsc(Long webtoonId);
+    @Query(value = "SELECT * FROM comment WHERE webtoon_id = :webtoon_id ORDER BY created_at ASC", nativeQuery = true)
+    List<Comment> findAllCommentByWebtoonId(@Param("webtoon_id") Long webtoonId);
 }
