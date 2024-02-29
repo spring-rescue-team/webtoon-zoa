0322ddcc0f90331cbf072f880540ac4bb2167c46
pastjung <87860163+pastjung@users.noreply.github.com>
2024-02-29 09:49:13 +0900

    fix: 웹툰 탐색 과정, CommentServiceTest 반영, 좋아요 설정/해지 후 검증과정 제외 (#12)
    * fix: checkExistWebtoonLikes 에서 웹툰 탐색 수정
    fix: CommentServiceTest 에서 findAllCommentByWebtoonId 반영
    fix: 좋아요 설정/해지 후 검증과정 제외
    
    * refactor: 코드 컨벤션 적용

--- src/main/java/com/project/webtoonzoa/repository/WebtoonRepository.java
+++ src/main/java/com/project/webtoonzoa/repository/WebtoonRepository.java
@@ -3,17 +3,11 @@ package com.project.webtoonzoa.repository;
 import com.project.webtoonzoa.entity.Webtoon;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface WebtoonRepository extends JpaRepository<Webtoon, Long> {
 
     List<Webtoon> findAllByDeletedAtIsNull();
 
     List<Webtoon> findTop5ByOrderByLikesDesc();
-
-    @Query("SELECT COUNT(wl) "
-        + "FROM WebtoonLikes wl "
-        + "WHERE wl.webtoon.id = :webtoonId")
-    Long countLikesByWebtoonId(Long webtoonId);
 }
 
